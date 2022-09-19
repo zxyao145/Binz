85e99c71720c1b65edfe08d8a7ff6925dfe92c81
zxyao <zxyao145@gmail.com>
2022-09-19 12:41:52 +0800

    chore: package

--- src/pack.cmd
+++ src/pack.cmd
@@ -1,4 +1,7 @@
 
+dotnet pack Binz.Core/Binz.Core.csproj --configuration Release --output "D:/source/localPackages/Binz/"
 dotnet pack Binz.Server/Binz.Server.csproj --configuration Release --output "D:/source/localPackages/Binz/"
-
 dotnet pack Binz.Client/Binz.Client.csproj --configuration Release --output "D:/source/localPackages/Binz/"
+dotnet pack Binz.Registry.Consul/Binz.Registry.Consul.csproj --configuration Release --output "D:/source/localPackages/Binz/"
+dotnet pack Binz.Registry.Etcd/Binz.Registry.Etcd.csproj --configuration Release --output "D:/source/localPackages/Binz/"
+
